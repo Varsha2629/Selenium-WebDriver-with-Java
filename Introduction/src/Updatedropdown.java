4be7867d1fe138a7d13cbd3d3612be3720ae203e
Varsha Panchal <varsha3051@gmail.com>
2020-06-24 12:06:43 -0400

    Real Time Exercises
    Learn Synchroniz, Calendar,

--- Introduction/src/Updatedropdown.java
+++ Introduction/src/Updatedropdown.java
@@ -1,7 +1,6 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 
 public class Updatedropdown {
 
@@ -11,17 +10,25 @@ public class Updatedropdown {
 		WebDriver driver = new ChromeDriver();
 
 		driver.get("http://spicejet.com"); // URL in the browser
-
-		Assert.assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		assertFalse(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 
 		// Assert.assertFalse(true);System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 
 		driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).click();
 
 		System.out.println(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+		assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
 
-		Assert.assertTrue(driver.findElement(By.cssSelector("input[id*='SeniorCitizenDiscount']")).isSelected());
+	}
 
+	private static void assertTrue(boolean selected) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void assertFalse(boolean selected) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
